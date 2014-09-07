3fffd4494d8f1149e7b4a7b2d45e93af5cb54b96
stockmopar <git@stockmopar.com>
2014-09-07 08:24:36 -0500

    Removed Config

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var util = require('util')
-  , stream = require('stream')
-  , configHandlers = require('./lib/config');
+  , stream = require('stream');
 
 // Give our module a stream interface
 util.inherits(localRules,stream);
@@ -33,36 +32,3 @@ function localRules(opts,app) {
 
 // Export it
 module.exports = localRules;
-
-/**
- * Add a particular TCP to this configuration
- * @param {[String} host Host of the TCP to remember
- */
-localRules.prototype.remember = function(host) {
-  this._app.log.info("(TCP Lights) remember()");
-  this._opts.sockets.push(host);
-  this.save();
-};
-
-/**
- * Called when a user prompts a configuration
- * @param  {Object}   rpc     RPC Object
- * @param  {Function} cb      Callback with return data
- */
-localRules.prototype.config = function(rpc,cb) {
-  this._app.log.info("(TCP Lights) config()");
-  var self = this;
-
-  if (!rpc) {
-    return configHandlers.probe.call(this,cb);
-  }
-
-  switch (rpc.method) {
-    case 'configScan':               return configHandlers.configScan.call(this,rpc.params,cb); break;
-    case 'manual_set_tcp':    return configHandlers.manual_set_tcp.call(this,rpc.params,cb); break;
-    case 'manual_get_tcp':    return configHandlers.manual_get_tcp.call(this,rpc.params,cb); break;
-    case 'manual_show_remove': return configHandlers.manual_show_remove.call(this,rpc.params,cb); break;
-    case 'manual_remove_tcp': return configHandlers.manual_remove_tcp.call(this,rpc.params,cb); break;
-    default:                   return cb(true);                                              break;
-  }
-};
